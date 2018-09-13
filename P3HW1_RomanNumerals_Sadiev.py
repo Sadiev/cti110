4d85bbfb9d41ee9d650fa7b03c1399b846691f4d
Dilshod Sadiev <36055195+Sadiev@users.noreply.github.com>
2018-09-13 09:36:02 -0400

    Add files via upload
    First submission

---
+++ P3HW1_RomanNumerals_Sadiev.py
@@ -0,0 +1,46 @@
+# CTI-110 
+# P3HW1 - Roman Numerals 
+# Sadiev 
+# 09/12/2018
+#
+
+print('I can display the Roman numeral version of the numbers 1 through 10')
+
+#Prompt the user to enter a number.
+numb=int(input('Please enter nuber within the range of 1 through 10: '))
+
+#Display the Roman numeral version of the number 
+if numb==1:
+    print('The Roman numeral version of the number is: I')
+elif numb==2:
+    print('The Roman numeral version of the number is: II')
+elif numb==3:
+    print('The Roman numeral version of the number is: III')
+elif numb==4:
+    print('The Roman numeral version of the number is: IV')
+elif numb==5:
+    print('The Roman numeral version of the number is: V')
+elif numb==6:
+    print('The Roman numeral version of the number is: VI')
+elif numb==7:
+    print('The Roman numeral version of the number is: VII')
+elif numb==8:
+    print('The Roman numeral version of the number is: VIII')
+elif numb==9:
+    print('The Roman numeral version of the number is: IX')
+elif numb==10:
+    print('The Roman numeral version of the number is: X')
+else:
+	#if the number is outside the range of 1 through 10, the program is display the arror massage
+    print('Error: The number you entered is outside the range')
+	
+input("Press any key to continue")
+
+
+
+
+
+
+    
+
+    
